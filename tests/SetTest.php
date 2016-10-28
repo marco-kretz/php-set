dc07eb761734a2399a3b4165c2235ffe550c12c5
Marco Kretz <mk@marco-kretz.de>
2016-10-28 13:58:34 +0200

    Added docs, functionality

--- tests/SetTest.php
+++ tests/SetTest.php
@@ -71,6 +71,64 @@ class SetTest extends PHPUnit_Framework_TestCase
         $this->set->add(5);
         $this->set->addAll($addItems);
 
-        $this->assertTrue(count($this->set) === 4);
+        $this->assertTrue(count($this->set) === 3);
+    }
+
+    public function testSetRemove1()
+    {
+        $initialItem = [1, 2, 3, 4, 5, 6];
+        $this->set->addAll($initialItem);
+        $this->set->remove(3);
+
+        $this->assertTrue(array_search(4, $this->set->toArray()) === 2);
+    }
+
+    public function testSetRemove2()
+    {
+        $initialItem = [1, 2, 3, 4, 5, 6];
+        $this->set->addAll($initialItem);
+        $this->set->remove(3);
+
+        $this->assertTrue(array_search(3, $this->set->toArray()) === false);
+    }
+
+    public function testSetIsEmpty1()
+    {
+        $this->assertTrue($this->set->isEmpty());
+    }
+
+    public function testSetIsEmpty2()
+    {
+        $this->set->add(1);
+        $this->assertFalse($this->set->isEmpty());
+    }
+
+    public function testSetIsEmpty3()
+    {
+        $this->set->add("Hello");
+        $this->set->add("World");
+        $this->set->add(1337);
+        $this->assertFalse($this->set->isEmpty());
+    }
+
+    public function testSetEquals1()
+    {
+        $values = [5, 8, 'test'];
+        $compareSet = new Set();
+        $compareSet->addAll($values);
+        $this->set->addAll($values);
+
+        $this->assertTrue($this->set->equals($compareSet));
+    }
+
+    public function testSetEquals2()
+    {
+        $values = [5, 8, 'test'];
+        $compareSet = new Set();
+        $compareSet->add('fail');
+        $compareSet->addAll($values);
+        $this->set->addAll($values);
+
+        $this->assertFalse($this->set->equals($compareSet));
     }
 }
